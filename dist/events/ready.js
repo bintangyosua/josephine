f8ed5a56110894748a6f6b14f0f13846ab6cd017
Bintang <itzbintangyosua@gmail.com>
2025-05-21 19:54:20 +0700

    update: update activity bot

--- dist/events/ready.js
+++ dist/events/ready.js
@@ -7,5 +7,22 @@ exports.default = {
     once: true,
     execute(client) {
         logger_1.logger.info(`🤖 Logged in as ${client.user?.tag}`);
+        const activities = [
+            { name: "Netflix and chill", type: discord_js_1.ActivityType.Playing },
+            { name: "Coding with coffee", type: discord_js_1.ActivityType.Listening },
+            { name: "YouTube drama", type: discord_js_1.ActivityType.Watching },
+            {
+                name: "Valorant live",
+                type: discord_js_1.ActivityType.Streaming,
+                url: "https://twitch.tv/yourchannel",
+            },
+            { name: "Hackathon mode", type: discord_js_1.ActivityType.Competing },
+        ];
+        let index = 0;
+        setInterval(() => {
+            const activity = activities[index];
+            client.user?.setActivity(activity.name, { type: activity.type });
+            index = (index + 1) % activities.length;
+        }, 1000); // ganti setiap 10 detik
     },
 };
